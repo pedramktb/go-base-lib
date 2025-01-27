e731975543c34a609154efaa46350e7c65dc2d71
pedramktb <79080845+pedramktb@users.noreply.github.com>
2025-01-27 22:12:50 +0100

    feat: implement WireGuard manager with routing and interface management

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/pedramktb/go-base-lib
 
-go 1.23.4
+go 1.23.5
 
 require (
+	github.com/coreos/go-iptables v0.8.0
 	github.com/joho/godotenv v1.5.1
 	github.com/pkg/sftp v1.13.7
 	go.mongodb.org/mongo-driver v1.17.2
@@ -12,15 +13,23 @@ require (
 
 require (
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/josharian/native v1.1.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/mdlayher/genetlink v1.3.2 // indirect
+	github.com/mdlayher/netlink v1.7.2 // indirect
+	github.com/mdlayher/socket v0.5.1 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	golang.zx2c4.com/wireguard v0.0.0-20231211153847-12269c276173 // indirect
+	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20241231184526-a9ab2273dd10
 )
---
+++ pkg/wireguard/routing_linux.go
@@ -0,0 +1,87 @@
+//go:build linux
+
+package wireguard
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/coreos/go-iptables/iptables"
+)
+
+func (m *Manager) addRouting() error {
+	ipt, err := iptables.NewWithProtocol(iptables.ProtocolIPv4)
+	if err != nil {
+		return fmt.Errorf("failed to create iptables client: %w", err)
+	}
+
+	err = ipt.Insert("nat", "POSTROUTING", 1, "-s", m.netV4.String(), "-j", "MASQUERADE")
+	if err != nil {
+		return fmt.Errorf("failed to add masquerade rule for ipv4: %w", err)
+	}
+
+	err = ipt.Insert("nat", "PREROUTING", 1, "-d", m.addrV4.String(), "-p", "udp", "--dport", "53", "-j", "DNAT", "--to-destination", m.dnsV4.String())
+	if err != nil {
+		return fmt.Errorf("failed to add dns rule for ipv4: %w", err)
+	}
+
+	ip6t, err := iptables.NewWithProtocol(iptables.ProtocolIPv6)
+	if err != nil {
+		return fmt.Errorf("failed to create iptables client: %w", err)
+	}
+
+	err = ip6t.Insert("nat", "POSTROUTING", 1, "-s", m.netV6.String(), "-j", "MASQUERADE")
+	if err != nil {
+		return fmt.Errorf("failed to add masquerade rule for ipv6: %w", err)
+	}
+
+	err = ip6t.Insert("nat", "PREROUTING", 1, "-d", m.addrV4.String(), "-p", "udp", "--dport", "53", "-j", "DNAT", "--to-destination", fmt.Sprintf("[%s]", m.dnsV6.String()))
+	if err != nil {
+		return fmt.Errorf("failed to add dns rule for ipv6: %w", err)
+	}
+
+	return nil
+}
+
+func (m *Manager) removeRouting() error {
+	var errs []error
+
+	ipt, err := iptables.NewWithProtocol(iptables.ProtocolIPv4)
+	if err != nil {
+		errs = append(errs, fmt.Errorf("failed to create iptables client: %w", err))
+	}
+
+	if ipt != nil {
+		err = ipt.DeleteIfExists("nat", "POSTROUTING", "-s", m.netV4.String(), "-j", "MASQUERADE")
+		if err != nil {
+			errs = append(errs, fmt.Errorf("failed to delete masquerade rule for ipv4: %w", err))
+		}
+
+		err = ipt.DeleteIfExists("nat", "PREROUTING", "-d", m.addrV4.String(), "-p", "udp", "--dport", "53", "-j", "DNAT", "--to-destination", m.dnsV4.String())
+		if err != nil {
+			errs = append(errs, fmt.Errorf("failed to delete dns rule for ipv4: %w", err))
+		}
+	}
+
+	ip6t, err := iptables.NewWithProtocol(iptables.ProtocolIPv6)
+	if err != nil {
+		errs = append(errs, fmt.Errorf("failed to create iptables client: %w", err))
+	}
+
+	if ip6t != nil {
+		err = ip6t.DeleteIfExists("nat", "POSTROUTING", "-s", m.netV6.String(), "-j", "MASQUERADE")
+		if err != nil {
+			errs = append(errs, fmt.Errorf("failed to delete masquerade rule for ipv6: %w", err))
+		}
+
+		err = ip6t.DeleteIfExists("nat", "PREROUTING", "-d", m.addrV4.String(), "-p", "udp", "--dport", "53", "-j", "DNAT", "--to-destination", fmt.Sprintf("[%s]", m.dnsV6.String()))
+		if err != nil {
+			errs = append(errs, fmt.Errorf("failed to delete dns rule for ipv6: %w", err))
+		}
+	}
+
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+	return nil
+}
